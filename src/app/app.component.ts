0cdec69b0bd1c0649a2d398d37850ca21cdb5a30
SDev03 <dev2523@hotmail.com>
2019-04-03 11:19:03 +0800

    comment tick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,6 @@ export class AppComponent implements OnInit {
 
   threeSecondTick() {
     this.ts3.stop(this.threeSecondTick.bind(this));
-    console.log('app.component.threeSecondTick!');
+    console.log('app.component.threeSecondTick!');  ///Tick
   }
 }
